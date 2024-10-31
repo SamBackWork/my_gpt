f1dab10c160ecb87765d5ebc6ff2d1bebd1cee70
SamBackWork <sambackwork@gmail.com>
2024-10-31 19:42:45 +0300

    подправил все думки и планы. Позже буду писать неплохие рекомендации.

--- condition_Linux.sh
+++ condition_Linux.sh
@@ -1,9 +1,9 @@
 if [ -d ".venv" ]; then
 	. .venv/bin/activate
 else
-	sudo install python3-venv
+	sudo apt-get install python3-venv
 	python3 -m venv .venv
-	. .venv/bin/actevate
+	. .venv/bin/activate
 	pip install -r requirements.txt
 fi
 python3 main.py
